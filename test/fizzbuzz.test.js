b9fe8b047d489bfe9be289db5bef2f0b89739a8b
Carlos Blanco <carlosblanco@me.com>
2022-10-01 22:41:06 +0200

    Add first test

---
+++ test/fizzbuzz.test.js
@@ -0,0 +1,12 @@
+import { describe, expect, it } from "vitest"
+
+const fizzbuzz = () => {
+
+}
+
+
+describe('fizzbuzz', () => {
+  it('should be a function', () => {
+    expect(typeof fizzbuzz).toBe('function')
+  })
+})
