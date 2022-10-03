c2bf9984f6a9326361103104aa6f058ac23cc69e
Carlos Blanco <carlosblanco@me.com>
2022-10-03 21:23:05 +0200

    Add test

--- test/fizzbuzz.test.js
+++ test/fizzbuzz.test.js
@@ -1,7 +1,7 @@
 import { describe, expect, it } from "vitest"
 
-const fizzbuzz = (value) => {
-
+const fizzbuzz = (number) => {
+ if (typeof number !== 'number') throw new Error()
 }
 
 
@@ -9,4 +9,9 @@ describe('fizzbuzz', () => {
   it('should be a function', () => {
     expect(typeof fizzbuzz).toBe('function')
   })
+
+  it('it should throw if parameter is not a number', () => {
+    expect(() => fizzbuzz()).toThrow()
+  })
+
 })
